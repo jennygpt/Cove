a1fc8b1256d18341a92fae3d1b74077a2614fb01
jennygpt <grout.coax_9i@icloud.com>
2025-06-30 18:14:50 -0700

    docs: clarify env setup

---
+++ utils/weather.js
@@ -0,0 +1,8 @@
+const axios = require('axios');
+
+async function getWeather(location) {
+  // Placeholder implementation. Replace with real weather API calls.
+  return `Weather info for ${location}`;
+}
+
+module.exports = { getWeather };
